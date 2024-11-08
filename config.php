0965e2299ef0c2f8a4c70cc6f5b8329c4d5df77a
HOUSSEINOMARSALLOUM <housseinsalloum0@gmail.com>
2024-11-08 05:58:30 +0200

    Created config,php for db configuration

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
+
+$host = 'localhost';
+$dbname = 'expense_tracker';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die(json_encode(['error' => 'Connection failed: ' . $e->getMessage()]));
+}
